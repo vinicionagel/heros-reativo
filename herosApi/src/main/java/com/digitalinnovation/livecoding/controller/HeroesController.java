904fe1182d9ca2120891191b2d2cb35b53c090df
vinicio.nagel <vinicionagel@gmail.com>
2021-04-27 20:22:52 -0300

    ajuste aplicacao

--- herosApi/src/main/java/com/digitalinnovation/livecoding/controller/HeroesController.java
+++ herosApi/src/main/java/com/digitalinnovation/livecoding/controller/HeroesController.java
@@ -11,54 +11,53 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
-
 import static com.digitalinnovation.livecoding.constants.HeroesConstant.HEROES_ENDPOINT_LOCAL;
 
 @RestController
 @Slf4j
 public class HeroesController {
-  HeroesService heroesService;
+    HeroesService heroesService;
 
-  HeroesRepository heroesRepository;
+    HeroesRepository heroesRepository;
 
-  private static final org.slf4j.Logger log =
-    org.slf4j.LoggerFactory.getLogger(HeroesController.class);
+    private static final org.slf4j.Logger log =
+            org.slf4j.LoggerFactory.getLogger(HeroesController.class);
 
-  public HeroesController(HeroesService heroesService, HeroesRepository heroesRepository) {
-    this.heroesService = heroesService;
-    this.heroesRepository = heroesRepository;
-  }
+    public HeroesController(HeroesService heroesService, HeroesRepository heroesRepository) {
+        this.heroesService = heroesService;
+        this.heroesRepository = heroesRepository;
+    }
 
-  @GetMapping(HEROES_ENDPOINT_LOCAL)
-  @ResponseStatus(HttpStatus.OK)
-  public Flux<Heroes> getAllItems() {
-    log.info("requesting the list off all heroes");
-    return heroesService.findAll();
+    @GetMapping(HEROES_ENDPOINT_LOCAL)
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<Heroes> getAllItems() {
+        log.info("requesting the list off all heroes");
+        return heroesService.findAll();
 
-  }
+    }
 
 
-  @GetMapping(HEROES_ENDPOINT_LOCAL + "/{id}")
-  public Mono<ResponseEntity<Heroes>> findByIdHero(@PathVariable String id) {
-    log.info("Requesting the hero with id {}", id);
-    return heroesService.findByIdHero(id)
-      .map((item) -> new ResponseEntity<>(item, HttpStatus.OK))
-      .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-  }
+    @GetMapping(HEROES_ENDPOINT_LOCAL + "/{id}")
+    public Mono<ResponseEntity<Heroes>> findByIdHero(@PathVariable String id) {
+        log.info("Requesting the hero with id {}", id);
+        return heroesService.findByIdHero(id)
+                .map((item) -> new ResponseEntity<>(item, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 
-  @PostMapping(HEROES_ENDPOINT_LOCAL)
-  @ResponseStatus(HttpStatus.CREATED)
-  public Mono<Heroes> createHero(@RequestBody Heroes heroes) {
-    log.info("A new Hero was Created");
-    return heroesService.save(heroes);
+    @PostMapping(HEROES_ENDPOINT_LOCAL)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Heroes> createHero(@RequestBody Heroes heroes) {
+        log.info("A new Hero was Created");
+        return heroesService.save(heroes);
 
-  }
+    }
 
-  @DeleteMapping(HEROES_ENDPOINT_LOCAL + "/{id}")
-  @ResponseStatus(code = HttpStatus.NOT_FOUND)
-  public Mono<HttpStatus> deletebyIDHero(@PathVariable String id) {
-    heroesService.deletebyIDHero(id);
-    log.info("Deleting the hero with id {}", id);
-    return Mono.just(HttpStatus.NOT_FOUND);
-  }
+    @DeleteMapping(HEROES_ENDPOINT_LOCAL + "/{id}")
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public Mono<HttpStatus> deletebyIDHero(@PathVariable String id) {
+        heroesService.deletebyIDHero(id);
+        log.info("Deleting the hero with id {}", id);
+        return Mono.just(HttpStatus.NOT_FOUND);
+    }
 }
